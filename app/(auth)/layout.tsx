63919073ffe1e5611473ec5f8b14fc834dfda157
Sergio Beaumont <xx830@globalia.com>
2023-09-04 16:09:54 +0100

    Auth with clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AuthLayout({ children } : { children: React.ReactNode}) {
   return (
-    <div className="bg-red-500 h-full">
+    <div className="h-full flex items-center justify-center">
       {children}
     </div>
   )
