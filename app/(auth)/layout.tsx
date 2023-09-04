83e5a58da015d5a41c33cbf79f074858bd863d95
Sergio Beaumont <xx830@globalia.com>
2023-09-04 15:47:30 +0100

    Reestructuring project

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthLayout({ children } : { children: React.ReactNode}) {
+  return (
+    <div className="bg-red-500 h-full">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
