5a3ff310589c66c91fac96efb82992449087d979
Sergio Beaumont <xx830@globalia.com>
2023-09-04 16:29:00 +0100

    Dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import { Button } from '../../../components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '../../../components/mode-toggle'
 
 export default function Home() {
   return (
-    <div>
-      Protected Route...
+    <div className="flex gap-2 items-center p-2">
+      <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
     </div>
   )
 }
