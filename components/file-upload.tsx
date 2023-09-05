9b9d5b76951e10431b151a124178e7d0e2e187c8
Sergio Beaumont <xx830@globalia.com>
2023-09-05 11:21:22 +0100

    Image preview

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { X } from "lucide-react"
+import Image from 'next/image'
+
 import { UploadDropzone } from "@uploadthing/react"
 import '@uploadthing/react/styles.css'
 
@@ -10,11 +13,25 @@ interface IFileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: IFileUploadProps) => {
+
+  const fileType = value?.split('.').pop()
+
+  if(value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button type="button" onClick={() => onChange('')} className="bg-rose-500 text-white rounded-full absolute top-0 right-0 p-1 shadow-sm">
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone 
       endpoint={endpoint}
       onClientUploadComplete={(res: any) => {
-        onChange(res?.[0].fileUrl)
+        onChange(res?.[0].url)
       }}
       onUploadError={(error: Error) => {
         console.log(error)
