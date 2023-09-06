613db54d166a3f3107bd054940b6ff4e1201fa9a
Sergio Beaumont <xx830@globalia.com>
2023-09-06 18:50:38 +0100

    Roles and members management on server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -44,7 +44,7 @@ export const ServerHeader = ({ server, role }: IProps) => {
         }
 
         { isAdmin &&
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem onClick={() => onOpen('members', { server })} className="px-3 py-2 text-sm cursor-pointer">
             Manage Members
             <UserIcon className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
